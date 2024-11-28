8ab785973f934d5abb92bfc9a419c28010720119
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-28 13:12:53 -0300

    Add files via upload

---
+++ projeto_ClasseLEDs/ClasseLEDs/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ClasseLEDs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntLED1_ON = new System.Windows.Forms.Button();
+            this.btnLED2_ON = new System.Windows.Forms.Button();
+            this.btnLED3_ON = new System.Windows.Forms.Button();
+            this.btnLED4_ON = new System.Windows.Forms.Button();
+            this.btnLED5_ON = new System.Windows.Forms.Button();
+            this.btnLED6_ON = new System.Windows.Forms.Button();
+            this.btnLED7_ON = new System.Windows.Forms.Button();
+            this.btnLED8_ON = new System.Windows.Forms.Button();
+            this.btnLED8_OFF = new System.Windows.Forms.Button();
+            this.btnLED7_OFF = new System.Windows.Forms.Button();
+            this.btnLED6_OFF = new System.Windows.Forms.Button();
+            this.btnLED5_OFF = new System.Windows.Forms.Button();
+            this.btnLED4_OFF = new System.Windows.Forms.Button();
+            this.btnLED3_OFF = new System.Windows.Forms.Button();
+            this.btnLED2_OFF = new System.Windows.Forms.Button();
+            this.btnLED1_OFF = new System.Windows.Forms.Button();
+            this.txtDado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bntLED1_ON
+            // 
+            this.bntLED1_ON.Location = new System.Drawing.Point(52, 77);
+            this.bntLED1_ON.Name = "bntLED1_ON";
+            this.bntLED1_ON.Size = new System.Drawing.Size(75, 23);
+            this.bntLED1_ON.TabIndex = 0;
+            this.bntLED1_ON.Text = "1_ON";
+            this.bntLED1_ON.UseVisualStyleBackColor = true;
+            this.bntLED1_ON.Click += new System.EventHandler(this.bntLED1_ON_Click);
+            // 
+            // btnLED2_ON
+            // 
+            this.btnLED2_ON.Location = new System.Drawing.Point(133, 77);
+            this.btnLED2_ON.Name = "btnLED2_ON";
+            this.btnLED2_ON.Size = new System.Drawing.Size(75, 23);
+            this.btnLED2_ON.TabIndex = 1;
+            this.btnLED2_ON.Text = "2_ON";
+            this.btnLED2_ON.UseVisualStyleBackColor = true;
+            this.btnLED2_ON.Click += new System.EventHandler(this.btnLED2_ON_Click);
+            // 
+            // btnLED3_ON
+            // 
+            this.btnLED3_ON.Location = new System.Drawing.Point(214, 77);
+            this.btnLED3_ON.Name = "btnLED3_ON";
+            this.btnLED3_ON.Size = new System.Drawing.Size(75, 23);
+            this.btnLED3_ON.TabIndex = 2;
+            this.btnLED3_ON.Text = "3_ON";
+            this.btnLED3_ON.UseVisualStyleBackColor = true;
+            this.btnLED3_ON.Click += new System.EventHandler(this.btnLED3_ON_Click);
+            // 
+            // btnLED4_ON
+            // 
+            this.btnLED4_ON.Location = new System.Drawing.Point(295, 77);
+            this.btnLED4_ON.Name = "btnLED4_ON";
+            this.btnLED4_ON.Size = new System.Drawing.Size(75, 23);
+            this.btnLED4_ON.TabIndex = 3;
+            this.btnLED4_ON.Text = "4_ON";
+            this.btnLED4_ON.UseVisualStyleBackColor = true;
+            this.btnLED4_ON.Click += new System.EventHandler(this.btnLED4_ON_Click);
+            // 
+            // btnLED5_ON
+            // 
+            this.btnLED5_ON.Location = new System.Drawing.Point(376, 77);
+            this.btnLED5_ON.Name = "btnLED5_ON";
+            this.btnLED5_ON.Size = new System.Drawing.Size(75, 23);
+            this.btnLED5_ON.TabIndex = 4;
+            this.btnLED5_ON.Text = "5_ON";
+            this.btnLED5_ON.UseVisualStyleBackColor = true;
+            this.btnLED5_ON.Click += new System.EventHandler(this.btnLED5_ON_Click);
+            // 
+            // btnLED6_ON
+            // 
+            this.btnLED6_ON.Location = new System.Drawing.Point(457, 77);
+            this.btnLED6_ON.Name = "btnLED6_ON";
+            this.btnLED6_ON.Size = new System.Drawing.Size(75, 23);
+            this.btnLED6_ON.TabIndex = 5;
+            this.btnLED6_ON.Text = "6_ON";
+            this.btnLED6_ON.UseVisualStyleBackColor = true;
+            this.btnLED6_ON.Click += new System.EventHandler(this.btnLED6_ON_Click);
+            // 
+            // btnLED7_ON
+            // 
+            this.btnLED7_ON.Location = new System.Drawing.Point(538, 77);
+            this.btnLED7_ON.Name = "btnLED7_ON";
+            this.btnLED7_ON.Size = new System.Drawing.Size(75, 23);
+            this.btnLED7_ON.TabIndex = 6;
+            this.btnLED7_ON.Text = "7_ON";
+            this.btnLED7_ON.UseVisualStyleBackColor = true;
+            this.btnLED7_ON.Click += new System.EventHandler(this.btnLED7_ON_Click);
+            // 
+            // btnLED8_ON
+            // 
+            this.btnLED8_ON.Location = new System.Drawing.Point(619, 77);
+            this.btnLED8_ON.Name = "btnLED8_ON";
+            this.btnLED8_ON.Size = new System.Drawing.Size(75, 23);
+            this.btnLED8_ON.TabIndex = 7;
+            this.btnLED8_ON.Text = "8_ON";
+            this.btnLED8_ON.UseVisualStyleBackColor = true;
+            this.btnLED8_ON.Click += new System.EventHandler(this.btnLED8_ON_Click);
+            // 
+            // btnLED8_OFF
+            // 
+            this.btnLED8_OFF.Location = new System.Drawing.Point(52, 229);
+            this.btnLED8_OFF.Name = "btnLED8_OFF";
+            this.btnLED8_OFF.Size = new System.Drawing.Size(75, 23);
+            this.btnLED8_OFF.TabIndex = 8;
+            this.btnLED8_OFF.Text = "8_OFF";
+            this.btnLED8_OFF.UseVisualStyleBackColor = true;
+            this.btnLED8_OFF.Click += new System.EventHandler(this.btnLED8_OFF_Click);
+            // 
+            // btnLED7_OFF
+            // 
+            this.btnLED7_OFF.Location = new System.Drawing.Point(133, 229);
+            this.btnLED7_OFF.Name = "btnLED7_OFF";
+            this.btnLED7_OFF.Size = new System.Drawing.Size(75, 23);
+            this.btnLED7_OFF.TabIndex = 9;
+            this.btnLED7_OFF.Text = "7_OFF";
+            this.btnLED7_OFF.UseVisualStyleBackColor = true;
+            this.btnLED7_OFF.Click += new System.EventHandler(this.btnLED7_OFF_Click);
+            // 
+            // btnLED6_OFF
+            // 
+            this.btnLED6_OFF.Location = new System.Drawing.Point(214, 229);
+            this.btnLED6_OFF.Name = "btnLED6_OFF";
+            this.btnLED6_OFF.Size = new System.Drawing.Size(75, 23);
+            this.btnLED6_OFF.TabIndex = 10;
+            this.btnLED6_OFF.Text = "6_OFF";
+            this.btnLED6_OFF.UseVisualStyleBackColor = true;
+            this.btnLED6_OFF.Click += new System.EventHandler(this.btnLED6_OFF_Click);
+            // 
+            // btnLED5_OFF
+            // 
+            this.btnLED5_OFF.Location = new System.Drawing.Point(295, 229);
+            this.btnLED5_OFF.Name = "btnLED5_OFF";
+            this.btnLED5_OFF.Size = new System.Drawing.Size(75, 23);
+            this.btnLED5_OFF.TabIndex = 11;
+            this.btnLED5_OFF.Text = "5_OFF";
+            this.btnLED5_OFF.UseVisualStyleBackColor = true;
+            this.btnLED5_OFF.Click += new System.EventHandler(this.btnLED5_OFF_Click);
+            // 
+            // btnLED4_OFF
+            // 
+            this.btnLED4_OFF.Location = new System.Drawing.Point(376, 229);
+            this.btnLED4_OFF.Name = "btnLED4_OFF";
+            this.btnLED4_OFF.Size = new System.Drawing.Size(75, 23);
+            this.btnLED4_OFF.TabIndex = 12;
+            this.btnLED4_OFF.Text = "4_OFF";
+            this.btnLED4_OFF.UseVisualStyleBackColor = true;
+            this.btnLED4_OFF.Click += new System.EventHandler(this.btnLED4_OFF_Click);
+            // 
+            // btnLED3_OFF
+            // 
+            this.btnLED3_OFF.Location = new System.Drawing.Point(457, 229);
+            this.btnLED3_OFF.Name = "btnLED3_OFF";
+            this.btnLED3_OFF.Size = new System.Drawing.Size(75, 23);
+            this.btnLED3_OFF.TabIndex = 13;
+            this.btnLED3_OFF.Text = "3_OFF";
+            this.btnLED3_OFF.UseVisualStyleBackColor = true;
+            this.btnLED3_OFF.Click += new System.EventHandler(this.btnLED3_OFF_Click);
+            // 
+            // btnLED2_OFF
+            // 
+            this.btnLED2_OFF.Location = new System.Drawing.Point(538, 229);
+            this.btnLED2_OFF.Name = "btnLED2_OFF";
+            this.btnLED2_OFF.Size = new System.Drawing.Size(70, 23);
+            this.btnLED2_OFF.TabIndex = 14;
+            this.btnLED2_OFF.Text = "2_OFF";
+            this.btnLED2_OFF.UseVisualStyleBackColor = true;
+            this.btnLED2_OFF.Click += new System.EventHandler(this.btnLED2_OFF_Click);
+            // 
+            // btnLED1_OFF
+            // 
+            this.btnLED1_OFF.Location = new System.Drawing.Point(619, 229);
+            this.btnLED1_OFF.Name = "btnLED1_OFF";
+            this.btnLED1_OFF.Size = new System.Drawing.Size(75, 23);
+            this.btnLED1_OFF.TabIndex = 15;
+            this.btnLED1_OFF.Text = "1_OFF";
+            this.btnLED1_OFF.UseVisualStyleBackColor = true;
+            this.btnLED1_OFF.Click += new System.EventHandler(this.btnLED1_OFF_Click);
+            // 
+            // txtDado
+            // 
+            this.txtDado.Location = new System.Drawing.Point(52, 286);
+            this.txtDado.Name = "txtDado";
+            this.txtDado.Size = new System.Drawing.Size(100, 20);
+            this.txtDado.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDado);
+            this.Controls.Add(this.btnLED1_OFF);
+            this.Controls.Add(this.btnLED2_OFF);
+            this.Controls.Add(this.btnLED3_OFF);
+            this.Controls.Add(this.btnLED4_OFF);
+            this.Controls.Add(this.btnLED5_OFF);
+            this.Controls.Add(this.btnLED6_OFF);
+            this.Controls.Add(this.btnLED7_OFF);
+            this.Controls.Add(this.btnLED8_OFF);
+            this.Controls.Add(this.btnLED8_ON);
+            this.Controls.Add(this.btnLED7_ON);
+            this.Controls.Add(this.btnLED6_ON);
+            this.Controls.Add(this.btnLED5_ON);
+            this.Controls.Add(this.btnLED4_ON);
+            this.Controls.Add(this.btnLED3_ON);
+            this.Controls.Add(this.btnLED2_ON);
+            this.Controls.Add(this.bntLED1_ON);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntLED1_ON;
+        private System.Windows.Forms.Button btnLED2_ON;
+        private System.Windows.Forms.Button btnLED3_ON;
+        private System.Windows.Forms.Button btnLED4_ON;
+        private System.Windows.Forms.Button btnLED5_ON;
+        private System.Windows.Forms.Button btnLED6_ON;
+        private System.Windows.Forms.Button btnLED7_ON;
+        private System.Windows.Forms.Button btnLED8_ON;
+        private System.Windows.Forms.Button btnLED8_OFF;
+        private System.Windows.Forms.Button btnLED7_OFF;
+        private System.Windows.Forms.Button btnLED6_OFF;
+        private System.Windows.Forms.Button btnLED5_OFF;
+        private System.Windows.Forms.Button btnLED4_OFF;
+        private System.Windows.Forms.Button btnLED3_OFF;
+        private System.Windows.Forms.Button btnLED2_OFF;
+        private System.Windows.Forms.Button btnLED1_OFF;
+        private System.Windows.Forms.TextBox txtDado;
+    }
+}
+
---
+++ projeto_ClasseLEDs/ClasseLEDs/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClasseLEDs
+{
+    public partial class Form1 : Form
+    {
+        private Leds meusLeds;
+        public Form1()
+        {
+            InitializeComponent();
+            meusLeds = new Leds();
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //
+
+        //ON
+        private void bntLED1_ON_Click(object sender, EventArgs e)
+        {
+            meusLeds.acender(1);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED2_ON_Click(object sender, EventArgs e)
+        {
+            meusLeds.acender(2);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED3_ON_Click(object sender, EventArgs e)
+        {
+            meusLeds.acender(3);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED4_ON_Click(object sender, EventArgs e)
+        {
+            meusLeds.acender(4);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED5_ON_Click(object sender, EventArgs e)
+        {
+            meusLeds.acender(5);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED6_ON_Click(object sender, EventArgs e)
+        {
+            meusLeds.acender(6);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED7_ON_Click(object sender, EventArgs e)
+        {
+            meusLeds.acender(7);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED8_ON_Click(object sender, EventArgs e)
+        {
+            meusLeds.acender(8);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        //OFF
+        private void btnLED1_OFF_Click(object sender, EventArgs e)
+        {
+            meusLeds.apagar(1);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED2_OFF_Click(object sender, EventArgs e)
+        {
+            meusLeds.apagar(2);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED3_OFF_Click(object sender, EventArgs e)
+        {
+            meusLeds.apagar(3);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED4_OFF_Click(object sender, EventArgs e)
+        {
+            meusLeds.apagar(4);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED5_OFF_Click(object sender, EventArgs e)
+        {
+            meusLeds.apagar(5);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED6_OFF_Click(object sender, EventArgs e)
+        {
+            meusLeds.apagar(6);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED7_OFF_Click(object sender, EventArgs e)
+        {
+            meusLeds.apagar(7);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+
+        private void btnLED8_OFF_Click(object sender, EventArgs e)
+        {
+            meusLeds.apagar(8);
+            txtDado.Text = meusLeds.getDado().ToString();
+        }
+    }
+}
